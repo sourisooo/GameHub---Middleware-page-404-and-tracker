649957b1663f7c9be98ba574d9be556ae1f1eb46
sourisooo <119761249+sourisooo@users.noreply.github.com>
2023-12-06 20:13:57 +0100

    Update page404.js

--- middleware/page404.js
+++ middleware/page404.js
@@ -5,23 +5,38 @@ const games = require('../games.json');
 
 const page404 = (req,res,next) => {
 
+    const { param } = req.params;
+
+    console.log("line10"+param,param===undefined,req.url,req.url==='/',param===undefined);
+
     const { gameName } = req.params;
 
-    // const gameFound = games.find((game) => game.name === gameName);
+    console.log("line10"+gameName,gameName===undefined,req.url,req.url==='/',((req.url).slice(0,6)==="/game/"));
+
+
+      if(req.url==='/view404') {res.render('view404')} else 
+    
+     if ((req.url==='/')) next();
+
+      
+     if(((req.url).slice(0,6)!=='/game/')&&(param!==undefined)) { res.redirect('/view404')} 
+
+
+    if(gameName===undefined) {next()} else {
+
 
     const gameFound = games.find((game) => {return game.name === gameName})
 
-    // console.log("gamef:"+ gameFound);
+
 
     if(gameFound) {
         res.render(gameName);
       } else {
-        // Sinon, je renvoie une 404
-        res.redirect('/view404');
+    
+        res.redirect('/view404')
       }
 
-    next();
-
+    ;}      
 
 }
 
